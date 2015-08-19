a9f6838beb50a4bcb589cf6f5f6044b565aa0827
Frank Xu Kun <xukun6300@gmail.com>
2015-08-19 15:15:58 +0800

    create user security question

--- FBSService/src/main/java/sg/com/fbs/services/system/security/uam/mgr/UserAccountManager.java
+++ FBSService/src/main/java/sg/com/fbs/services/system/security/uam/mgr/UserAccountManager.java
@@ -1,5 +1,11 @@
 package sg.com.fbs.services.system.security.uam.mgr;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import javax.annotation.Resource;
 
 import org.apache.commons.lang.StringUtils;
@@ -10,7 +16,9 @@ import sg.com.fbs.core.businfra.facade.CommonFacade;
 import sg.com.fbs.core.businfra.facade.ServiceLocator;
 import sg.com.fbs.core.techinfra.persistence.exception.DataAccessObjectException;
 import sg.com.fbs.model.system.persistence.response.ResponseCRUD;
+import sg.com.fbs.model.system.security.SecurityQuestions;
 import sg.com.fbs.model.system.security.User;
+import sg.com.fbs.model.system.security.UserSecurityQuestion;
 import sg.com.fbs.model.system.security.uam.RegisterUserRequest;
 import sg.com.fbs.services.security.external.crypto.provider.CryptoServicesClientIF;
 import sg.com.fbs.services.security.password.PasswordServices;
@@ -52,6 +60,34 @@ public class UserAccountManager extends CommonFacade{
 			user.setMobileNum(registerUserRequest.getMobileNo());
 		}
 		user.setStatus(RegisterUserEnum.PENDING_ACTIVATION.toString());
+		
+		//String[] secQuestionIds = {registerUserRequest.getSecurityQuestion1(), registerUserRequest.getSecurityQuestion2()};
+		String[] secQuestionIds = {"1000","1001"};
+		String[] answer = {registerUserRequest.getEncryptedAnswer1(), registerUserRequest.getEncryptedAnswer2()};
+		
+		Long[] longSecQuestionIds = new Long[secQuestionIds.length];
+		Set<UserSecurityQuestion> userQuestionSet = new HashSet<UserSecurityQuestion>();
+		
+		for (int i = 0; i < secQuestionIds.length; i++) {
+			if(secQuestionIds[i]!=null && !secQuestionIds[i].equals("-1")){
+				UserSecurityQuestion userSecurityQuestion = new UserSecurityQuestion();
+				SecurityQuestions questions = new SecurityQuestions();
+				longSecQuestionIds[i] = Long.parseLong(secQuestionIds[i]);
+				questions.setId(longSecQuestionIds[i]);
+				userSecurityQuestion.setQuestionId(questions);
+				userSecurityQuestion.setAnswer(answer[i]);
+				//List<String> decryptedAnswers = passwordServices.decryptSecurityAnswers(Arrays.asList(answer[i]));
+				List<String> decryptedAnswers = passwordServices.decryptSecurityAnswers(new ArrayList<String>(Arrays.asList(answer[i])));
+				if (decryptedAnswers != null && decryptedAnswers.size() > 0) {
+					for (String decryptedAnswer : decryptedAnswers) {
+						userSecurityQuestion.setAnswer(decryptedAnswer);
+					}
+				}
+				userQuestionSet.add(userSecurityQuestion);
+			}
+		}
+		user.setUserSecurityQuestion(userQuestionSet);
+		
 		user.setPasswordLastUpdateDate(new DateTime());
 		
 		String password = registerUserRequest.getPassword();
