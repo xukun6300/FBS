c9cad954c39bbd1c1cbccf38166085984b092c3a
Frank Xu Kun <xukun6300@gmail.com>
2015-08-25 17:29:24 +0800

    [ANA]log in authentication

---
+++ FBSWeb/src/main/java/sg/com/fbs/security/web/auth/fbsid/FbsIdAuthenticationProvider.java
@@ -0,0 +1,63 @@
+package sg.com.fbs.security.web.auth.fbsid;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.util.StringUtils;
+
+import sg.com.fbs.model.system.security.User;
+import sg.com.fbs.services.security.password.PasswordServices;
+import sg.com.fbs.services.system.security.uam.exception.UserAccountManagementException;
+import sg.com.fbs.services.system.security.uam.mgr.UserAccountManagerBD;
+
+/**
+ * @Author Frank Xu $
+ * @Created 6:00:28 pm 24 Aug, 2015 $
+ * Copyright (c) 2015 Financial & Budgeting System All Rights Reserved.
+ */
+public class FbsIdAuthenticationProvider implements AuthenticationProvider{
+
+	@Autowired
+	private PasswordServices passwordServices;
+	
+	
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		final UsernamePasswordAuthenticationToken userToken = (UsernamePasswordAuthenticationToken) authentication;
+		String username = userToken.getName();
+		String password = (String) authentication.getCredentials();
+		
+		if(!StringUtils.hasLength(username)){
+			throw new BadCredentialsException("Empty Username");
+		}
+		
+		FbsIdAuthenticationToken authToken = (FbsIdAuthenticationToken) authentication;
+		
+		User user =null;
+		Map<String, Object> map = new Hashtable<String, Object>();
+		UserAccountManagerBD userAccountManagerBD = new UserAccountManagerBD();
+		try {
+			userAccountManagerBD.getUserByLoginId(username);
+		} catch (UserAccountManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+	
+		
+		return false;
+	}
+
+}
---
+++ FBSWebPortal/src/main/webapp/WEB-INF/jsp/security/login.jsp
@@ -0,0 +1,12 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ taglib prefix="joda" uri="http://www.joda.org/joda/time/tags"%>
+<%@ taglib prefix="netui" uri="/tags/netui"%>
+<%@ taglib prefix="captcha" uri="/WEB-INF/tags/captcha.tld"%>
+
+
+
+
